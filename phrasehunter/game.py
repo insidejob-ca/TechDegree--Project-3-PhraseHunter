200059a349bd91217ecd3df3cce0fea4b398ffeb
insidejob-ca <chiragailani1805@gmail.com>
2021-09-14 18:21:28 -0700

    fixed issue with app

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -9,41 +9,38 @@ class Game:
                         Phrase('learning python is'), 
                         Phrase('lion is king'), 
                         Phrase('let and let live')]
-        self.active_phrases = None
+        self.active_phrase = None
         self.guesses = []
     
     
     def start(self):
         self.welcome()
-        self.active_phrases = self.get_random_phrase()
-        while self.missed < 5 and self.active_phrases.check_complete(self.guesses) == False:
-            print(f"Incorrect guesses : {self.missed}")
-            self.active_phrases.display(self.guesses)
+        self.active_phrase = self.get_random_phrase()
+        while self.missed < 5 and not self.active_phrase.check_complete(self.guesses):
+            self.active_phrase.display(self.guesses)
             print("\n")
-#            self.handle_guess()
             user_guess = self.get_guess()
             self.guesses.append(user_guess) # append user guess to the guesses list
-            self.active_phrases.check_letter(user_guess)
-#            self.handle_guess()
-            if not self.active_phrases.check_letter(user_guess):
-#                self.missed += 1
-                print(f"Incorrect guesses: {self.missed}")
-        print('The phrase was:', self.active_phrases.phrase.upper())        
+            self.active_phrase.check_letter(user_guess)
+            if not self.active_phrase.check_letter(user_guess):
+                print(f"Incorrect guesses: {self.missed}\n")
         self.game_over()
+        print('The phrase was:', self.active_phrase.phrase.upper()) 
         self.continue_playing()
+
     
     def get_random_phrase(self):
         return random.choice(self.phrases)
         
     
-
     def welcome(self):
         welcome_title = 'WELCOME TO PHRASEHUNTING GAME'
-        print( '=' * len(welcome_title), '{}'.format(welcome_title),'=' * len(welcome_title))
+        print('\n', '=' * len(welcome_title), '{}'.format(welcome_title),'=' * len(welcome_title), '\n')
         
-        
+            
     def get_guess(self):
         user_guess = input("Enter the guessing letter for guessing the guess phrase?  ")
+#       print('\n')
         user_guess = user_guess.lower()
         while True:
             if user_guess.isalpha():
@@ -58,38 +55,30 @@ class Game:
                 user_guess = self.get_guess()
                 continue                
         if user_guess in self.guesses:
-            print(f'You already guessed `{user_guess}`')
+            print(f'You already guessed "{user_guess}" Please Try again with a new letter')
         else:
             self.guesses.append(user_guess)
-            if self.active_phrases.check_letter(user_guess):
-                self.active_phrases.display(self.guesses)
-                print('\n')
+            if self.active_phrase.check_letter(user_guess):
+               print('\n')
             else:
-                print("You have guessed an incorrect letter. Please Try again with a new letter.")
+                print("You have guessed an incorrect letter. Please Try again.")
                 self.missed += 1
-                print("You are left with {} out of 5 chances to win this round!".format((5 - self.missed)))
-#        self.display()      
-        return user_guess            
-            
-    def handle_guess(self):
-        try:
-            result = self.active_phrases.check_letter(self.get_guess())
-            if result is False:
-                self.missed += 1
-        except ValueError as err:
-            print(err)    
+                print("######## You are left with {} out of 5 chances to win this round! ########\n".format((5 - self.missed)))
+        return user_guess 
     
+
     def game_over(self):
+        self.active_phrase.display(self.guesses)
         if self.missed == 5:
-            print('All the chances are over for guessing the phrase, GAME OVER!') 
-        else: 
-            print('Congrats, You guessed it right!!!')
+            print('\nAll the chances are over for guessing the phrase, GAME OVER!\n')
+        else:
+            print('\nCongrats, You guessed it right!!!\n')
             
             
     def continue_playing(self):
         user_continuing = input('Would you like to continue playing(Y(es)/N(o)?  ')
         if user_continuing.lower() == 'y':
-            self.active_phrases.reset()
+            self.active_phrase = None
             print('\n')
             new_game = Game()
             new_game.start()
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -5,7 +5,8 @@ class Phrase:
     
     def __init__(self, phrase):
         self.phrase = phrase.lower()
-
+        self.unique_letters = set(self.phrase)
+        self.unique_letters.discard(' ')
 
     def display(self, guesses):
         for letter_guess in self.phrase:
@@ -15,21 +16,16 @@ class Phrase:
                 print(f'{letter_guess}', end=" ")
             else:
                 print("_", end=" ")   
-        
-    
 
     def check_letter(self, guess):
         if guess in self.phrase:
             return True
         else:
-            return False
-        
+            return False        
+    
+#    def check_letter(self, guess):
+#        return guess in self.phrase
         
     def check_complete(self, guesses):
-        for letter_guess in self.phrase:
-            if letter_guess not in guesses:
-                return False
+        if len(self.unique_letters) == len(set(guesses)):
             return True
-         
-    def reset(self):
-        self.active_phrases = None
